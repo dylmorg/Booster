883f8136709f0b6394178f39535b8517f5ca4da8
Dylan Morgan <Dylan.Morgan@wolterskluwer.com>
2023-11-22 14:49:23 -0600

    Removing files from .txt

--- Booster.py
+++ Booster.py
@@ -1,1 +1,27 @@
-print ('hello world')
+import os
+
+cubeFile = "cube.txt"
+
+data = []
+poolList = []
+with open(cubeFile) as cubeList:
+    for val in cubeList.readlines():
+        if val and val !="# mainboard" and val !="# sideboard" and val != "# maybeboard" and val != '':
+            val = val.strip('\n')
+            data.append(val) 
+
+
+for pool in os.listdir():
+    if pool.endswith(".txt") and not pool.startswith(cubeFile):
+        poolList.append(pool)
+
+for pool in poolList:
+    with open (pool) as currentPool:
+        for currentCard in currentPool.readlines():
+            currentCard = currentCard.strip('\n')
+            currentCard = currentCard.strip('1 ')
+            print (currentCard)
+            if (currentCard in data):
+                print (currentCard)
+                data.remove(currentCard)
+print (data)
