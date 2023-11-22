4779e8b9a8b415ae4d98163d22c9343b05613ca4
Dylan Morgan <Dylan.Morgan@wolterskluwer.com>
2023-11-22 16:26:06 -0600

    Booster tutor?

--- Booster.py
+++ Booster.py
@@ -1,32 +1,33 @@
 import os
 import random 
+import re 
 
 cubeFile = "cube.txt"
 packSize = 15
 
 bonusPack = []
 data = []
-poolList = [] 
+poolList = []
 with open(cubeFile) as cubeList:
     for val in cubeList.readlines():
-        if val and val !="# mainboard" and val !="# sideboard" and val != "# maybeboard" and val != '':
+        val = val.strip('\n')
+        if  not val.startswith("# ") and len(val)> 0:
             val = val.strip('\n')
             data.append(val) 
 
-
+print (data)
 for pool in os.listdir():
     if pool.endswith(".txt") and not pool.startswith(cubeFile):
         poolList.append(pool)
 
-for pool in poolList:
+for pool in poolList:  
     with open (pool) as currentPool:
         for currentCard in currentPool.readlines():
-            currentCard = currentCard.strip('\n')
-            currentCard = currentCard.strip('1 ')
-            print (currentCard)
-            if (currentCard in data):
-                print (currentCard)
-                data.remove(currentCard)
+            if (currentCard.find("\d ")):
+                currentCard =  re.sub("\d ", "", currentCard)
+                print(currentCard)
+                if (currentCard in data):
+                    data.remove(currentCard)
 print (data)
 
 for i in range (1, packSize):
